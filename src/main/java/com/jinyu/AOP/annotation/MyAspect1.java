e48e26c1b6285a7ad8731322f8422427c34f5622
jinyu.mei <jinyu.mei@lincomb.com>
2017-12-21 09:23:01 +0800

    @PointCut annotation

--- src/main/java/com/jinyu/AOP/annotation/MyAspect1.java
+++ src/main/java/com/jinyu/AOP/annotation/MyAspect1.java
@@ -3,6 +3,7 @@ package com.jinyu.AOP.annotation;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,14 +15,33 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class MyAspect1 {
 
-    @Before("execution(* com.jinyu.AOP.ProductService.doSomething(..))")
+//    @Before("execution(* com.jinyu.AOP.ProductService.doSomething(..))")
+//    public void doAccessCheck() {
+//        System.out.println("in before check");
+//    }
+//
+//    @After("execution(* com.jinyu.AOP.ProductService.doSomething(..))")
+//    public void checkAfter(){
+//        System.out.println("after check");
+//    }
+
+    /**
+     * 通知关联切点名
+     * */
+    @Before("doSomething()")
     public void doAccessCheck() {
         System.out.println("in before check");
     }
 
-    @After("execution(* com.jinyu.AOP.ProductService.doSomething(..))")
+    @After("doSomething()")
     public void checkAfter(){
         System.out.println("after check");
     }
+
+    /**
+     * 定义切点名
+     * */
+    @Pointcut("execution(* com.jinyu.AOP.ProductService.doSomething(..))")
+    public void doSomething(){}
 
 }
